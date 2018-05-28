54d3a8f3e0c7bccadb9f89c7b69f3b335a89ad2d
Miroslav Matejovsky <miroslav.matejovsky@wirecard.com>
2018-05-28 10:45:43 +0200

    Initial commit

---
+++ src/main/java/sk/bsmk/keycloak_social/rest/ReceiveTokenRequest.java
@@ -0,0 +1,14 @@
+package sk.bsmk.keycloak_social.rest;
+
+public class ReceiveTokenRequest {
+
+  private String token;
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+}
